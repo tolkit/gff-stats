c46fb398d6d1352a3cacaae2c61e6428a532f283
Max Brown <mb39@sanger.ac.uk>
2021-04-08 08:52:10 +0100

    More helpful output

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn main() {
     let matches = App::new("GFF stats")
         .version(clap::crate_version!())
         .author("Max Brown <mb39@sanger.ac.uk>")
-        .about("Extract GFF3 regions and compute statistics on them.")
+        .about("Extract GFF3 regions from a reference fasta and compute statistics on them.")
         .arg(
             Arg::with_name("gff")
                 .short("g")
@@ -31,19 +31,19 @@ fn main() {
                 .required(true)
                 .help("The reference fasta file."),
         )
-        // .arg(
-        //     Arg::with_name("sequences")
-        //         .short("s")
-        //         .long("sequences")
-        //         .required(false)
-        //         .default_value("true")
-        //         .help("Save the extracted CDS fasta sequences?"),
-        // )
+        .arg(
+            Arg::with_name("sequences")
+                .short("s")
+                .long("sequences")
+                .required(false)
+                .default_value("false")
+                .help("Save the extracted CDS fasta sequences?"),
+        )
         .get_matches();
     // parse command line options
     let input_gff = matches.value_of("gff").unwrap();
     let input_fasta = matches.value_of("fasta").unwrap();
-    //let save_sequences = value_t!(matches.value_of("sequences"), bool).unwrap_or_else(|e| e.exit());
+    let save_sequences = value_t!(matches.value_of("sequences"), bool).unwrap_or_else(|e| e.exit());
 
     let fasta_reader = fasta::Reader::from_file(input_fasta).expect("[-]\tPath invalid.");
 
@@ -51,35 +51,26 @@ fn main() {
     if let Err(e) = create_dir_all("./gff-stats/") {
         println!("[-]\tCreate directory error: {}", e.to_string());
     }
-    let extension = "fasta";
-
-    // create file
-    let file_name = format!("./gff-stats/{}{}", "CDS.", extension);
-    let gff_output = File::create(&file_name).expect("[-]\tUnable to create file");
-    let gff_output = LineWriter::new(gff_output);
-    let mut f = BufWriter::new(gff_output);
-
-    // iterate over the fasta records
-    // might have to do a manual loop.
 
     struct Output {
         fasta_id: String,
         start: usize,
         end: usize,
-        gc_four: f64,
+        stats: utils::utils::Stats,
+        gc_four_stats: utils::utils::FourFoldStats,
         seq: String,
     }
 
     // parallelise the processing of the fastas.
     let (sender, receiver) = channel();
-    println!("[+]\tProcessing the following fastas in parallel:");
+    println!("[+]\tProcessing the following fasta sequences in parallel:");
     fasta_reader
         .records()
         .par_bridge()
         .for_each_with(sender, |s, record| {
             let fasta_record = record.expect("[-]\tError during fasta record parsing.");
             println!(
-                "[+]\t\t{} - {}bp.",
+                "[+]\t\t{} - {}bp",
                 fasta_record.id(),
                 fasta_record.seq().len()
             );
@@ -121,27 +112,31 @@ fn main() {
 
                         if strandedness == Strand::Reverse {
                             let revcomp_slice_str = utils::utils::reverse_complement(slice_str);
+                            let seq_stats = utils::utils::whole_seq_stats(&revcomp_slice_str);
                             let four_deg_codons =
                                 utils::utils::gc_four_fold_deg_sites(&revcomp_slice_str);
-                            let gc_four = utils::utils::gc_four(four_deg_codons);
+                            let gc_four = utils::utils::four_fold_site_stats(four_deg_codons);
 
                             s.send(Output {
                                 fasta_id: fasta_record.id().to_string(),
                                 start: start,
                                 end: end,
-                                gc_four: gc_four,
+                                stats: seq_stats,
+                                gc_four_stats: gc_four,
                                 seq: revcomp_slice_str,
                             })
                             .expect("send!")
                         } else {
+                            let seq_stats = utils::utils::whole_seq_stats(&slice_str);
                             let four_deg_codons = utils::utils::gc_four_fold_deg_sites(&slice_str);
-                            let gc_four = utils::utils::gc_four(four_deg_codons);
+                            let gc_four = utils::utils::four_fold_site_stats(four_deg_codons);
 
                             s.send(Output {
                                 fasta_id: fasta_record.id().to_string(),
                                 start: start,
                                 end: end,
-                                gc_four: gc_four,
+                                stats: seq_stats,
+                                gc_four_stats: gc_four,
                                 seq: slice_str.to_owned(),
                             })
                             .expect("send!")
@@ -154,11 +149,45 @@ fn main() {
     let res: Vec<Output> = receiver.iter().collect();
 
     println!("[+]\tCollected output and writing.");
+
+    if save_sequences {
+        // create fasta file
+        let file_name = format!("./gff-stats/{}", "CDS.fasta");
+        let output_fasta = File::create(&file_name).expect("[-]\tUnable to create file");
+        let output_fasta = LineWriter::new(output_fasta);
+        let mut f = BufWriter::new(output_fasta);
+        // write fastas.
+        for i in &res {
+            writeln!(f, ">{}.{}-{}\n{}", i.fasta_id, i.start, i.end, i.seq)
+                .unwrap_or_else(|_| println!("[-]\tError in writing to file."));
+        }
+    }
+    // text file output for now.
+    let file_name2 = format!("./gff-stats/{}", "stats.txt");
+    let stats = File::create(&file_name2).expect("[-]\tUnable to create file");
+    let stats = LineWriter::new(stats);
+    let mut f2 = BufWriter::new(stats);
+    // write stats.
+    writeln!(
+        f2,
+        "ID,loc,four_fold_deg_gc_per,four_fold_deg_at_per,four_fold_deg_gc_skew,four_fold_deg_at_skew,gc_per,at_per,gc_skew,at_skew"
+    )
+    .unwrap_or_else(|_| println!("[-]\tError in writing to file."));
     for i in res {
         writeln!(
-            f,
-            ">{}.{}-{}:{}\n{}",
-            i.fasta_id, i.start, i.end, i.gc_four, i.seq
+            f2,
+            "{},{}-{},{},{},{},{},{},{},{},{}",
+            i.fasta_id,
+            i.start,
+            i.end,
+            i.gc_four_stats.gc_percent,
+            i.gc_four_stats.at_percent,
+            i.gc_four_stats.gc_skew,
+            i.gc_four_stats.at_skew,
+            i.stats.gc_percent,
+            i.stats.at_percent,
+            i.stats.gc_skew,
+            i.stats.at_skew
         )
         .unwrap_or_else(|_| println!("[-]\tError in writing to file."));
     }
--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,6 @@
 pub mod utils {
 
+    use std::collections::HashMap;
     use std::str;
 
     pub const FOURFOLD_DEG: [&str; 32] = [
@@ -8,31 +9,76 @@ pub mod utils {
         "CGA", "CGG", "GGT", "GGC", "GGA", "GGG",
     ];
 
-    pub fn gc_four(codons: Vec<&[u8]>) -> f64 {
+    // stats on the whole sequence
+    pub struct Stats {
+        pub gc_percent: f64,
+        pub at_percent: f64,
+        pub gc_skew: f64,
+        pub at_skew: f64,
+    }
+
+    pub fn whole_seq_stats(dna: &str) -> Stats {
+        // chars uses quite a bit more memory here
+        // but sequences should never be super long...
+        let map = dna.chars().fold(HashMap::new(), |mut map, c| {
+            *map.entry(c).or_insert(0) += 1;
+            map
+        });
+        let g_counts = map.get(&'G').unwrap_or(&0);
+        let c_counts = map.get(&'C').unwrap_or(&0);
+        let a_counts = map.get(&'A').unwrap_or(&0);
+        let t_counts = map.get(&'T').unwrap_or(&0);
+
+        Stats {
+            gc_percent: (g_counts + c_counts) as f64 / dna.len() as f64,
+            at_percent: (a_counts + t_counts) as f64 / dna.len() as f64,
+            gc_skew: (g_counts - c_counts) as f64 / (g_counts + c_counts) as f64,
+            at_skew: (a_counts - t_counts) as f64 / (a_counts + t_counts) as f64,
+        }
+    }
+
+    // stats on the 4-fold-degenerate-sites
+    pub struct FourFoldStats {
+        pub gc_percent: f64,
+        pub at_percent: f64,
+        pub gc_skew: f64,
+        pub at_skew: f64,
+    }
+    pub fn four_fold_site_stats(codons: Vec<&[u8]>) -> FourFoldStats {
         let codon_vec = codons
             .into_iter()
             .map(str::from_utf8)
             .collect::<Result<Vec<&str>, _>>()
             .unwrap_or(vec![]);
 
-        let mut gc_counts = 0;
+        let mut g_counts = 0;
+        let mut c_counts = 0;
+        let mut a_counts = 0;
+        let mut t_counts = 0;
 
         for codon in codon_vec.iter() {
             let split_tuple = codon.split_at(2);
-            if split_tuple.1 == "G" || split_tuple.1 == "C" {
-                gc_counts += 1;
+
+            match split_tuple.1 {
+                "G" => g_counts += 1,
+                "C" => c_counts += 1,
+                "A" => a_counts += 1,
+                "T" => t_counts += 1,
+                _ => (),
             }
         }
-        let gc_four_float = gc_counts as f64 / codon_vec.len() as f64;
-        gc_four_float
+        FourFoldStats {
+            gc_percent: (g_counts + c_counts) as f64 / codon_vec.len() as f64,
+            at_percent: (a_counts + t_counts) as f64 / codon_vec.len() as f64,
+            gc_skew: (g_counts - c_counts) as f64 / (g_counts + c_counts) as f64,
+            at_skew: (a_counts - t_counts) as f64 / (a_counts + t_counts) as f64,
+        }
     }
 
     // from the frame trimming in main.rs, the sequence should start with full codons
     pub fn gc_four_fold_deg_sites(dna: &str) -> Vec<&[u8]> {
-        //let four_fold_deg_sites = Vec::new();
-        // convert to uppercase here, so bytes are the same
-        let upper = dna.as_bytes();
-        let codon_iterator = upper.chunks(3);
+        let bytes = dna.as_bytes();
+        let codon_iterator = bytes.chunks(3);
         let mut collector = Vec::new();
 
         for codon in codon_iterator {
